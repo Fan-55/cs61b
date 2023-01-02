6c90b23a1770a00b6b054db696d6c57f222c09a7
Fan5 <openconvenience@gmail.com>
2023-01-02 23:08:05 +0800

    Add ArrayDeque class

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,96 @@
+package deque;
+
+// invariants:
+// 1. size: The number of items in the list should be size.
+// 2. addLast: The item will go to position nextLast.
+// 3. addFirst: The item will go to position nextFirst.
+// 3. resize: double the size of items array and copy from the first item which is at position (nextFirst + 1) if the
+// nextFirst is not the last item in the array. Otherwise, first item is at position 0.
+
+public class ArrayDeque<T> {
+    private T[] items;
+    private int size;
+    private int nextLast;
+    private int nextFirst;
+
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        size = 0;
+        nextFirst = 4;
+        nextLast = 5;
+    }
+
+
+    private void resize() {
+        T[] resizedItems = (T[]) new Object[items.length * 2];
+        int indexForCopying = getIndexOfFirstItem();
+        for (int i = 0; i < items.length; i++) {
+            resizedItems[i] = items[indexForCopying];
+            if (indexForCopying + 1 == items.length) {
+                indexForCopying = 0;
+            } else {
+                indexForCopying++;
+            }
+        }
+        nextFirst = resizedItems.length - 1;
+        nextLast = items.length;
+        items = resizedItems;
+    }
+
+    private int getIndexOfFirstItem() {
+        int indexOfFirstItem;
+        if (nextFirst == items.length - 1) {
+            indexOfFirstItem = 0;
+        } else {
+            indexOfFirstItem = nextFirst + 1;
+        }
+        return indexOfFirstItem;
+    }
+
+    public void addLast(T item) {
+        if (size == items.length) {
+            resize();
+        }
+        items[nextLast] = item;
+        if (nextLast + 1 == items.length) {
+            nextLast = 0;
+        } else {
+            nextLast++;
+        }
+        size++;
+    }
+
+    public void addFirst(T item) {
+        if (size == items.length) {
+            resize();
+        }
+        items[nextFirst] = item;
+        if (nextFirst == 0) {
+            nextFirst = items.length - 1;
+        } else {
+            nextFirst--;
+        }
+        size++;
+    }
+
+    public T get(int index) {
+        return items[index];
+    }
+
+    public boolean isEmpty() {
+        return size <= 0;
+    }
+
+    public void printDeque() {
+        int indexForPrinting = getIndexOfFirstItem();
+        for (int i = 0; i < size; i++) {
+            System.out.print(items[indexForPrinting] + " ");
+            if (indexForPrinting + 1 == items.length) {
+                indexForPrinting = 0;
+            } else {
+                indexForPrinting++;
+            }
+        }
+        System.out.println();
+    }
+}
